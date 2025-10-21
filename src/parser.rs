71e565d88356056602ec9629e7efbe3477a1bdf9
Dennis Lee <dl@aduna.ch>
2025-10-21 13:15:05 +0200

    backup

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
 
-use crate::lexer::Lexer;
+use crate::lexer::{Lexer, Token};
 
 pub struct Parser {
     lex: Lexer,
@@ -9,4 +9,18 @@ impl Parser {
     pub fn new(content: String) -> Self {
         Parser { lex: Lexer::new(content) }
     }
+}
+pub struct File {
+    name: String,
+    interface: Vec<Interface>,
+    implementation: String,
+    initialization: String,
+    finalization: String,
+}
+
+pub enum Interface {
+    imports(Vec<String>),
+    constant(Vec<(String, Token)>),
+    definition,
+    variable,
 }
